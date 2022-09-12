9cbd1f6050faceffb1e970d78ac38c6bc29a9eb0
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-09-12 12:53:20 +0300

    Create Present.java

---
+++ model/Present.java
@@ -0,0 +1,12 @@
+package by.epam.task11.main.model;
+
+public class Present {
+    Present(){
+
+
+    }
+
+    public static void table(){
+        System.out.println("Room number\tRooms\tFloor\tStreet\t\t\tSquare\tHouse Type");
+    }
+}
